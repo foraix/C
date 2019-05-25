1172f022092555ea248134fddec2423d5cae571c
YT <362591904@qq.com>
2019-05-25 22:18:53 +0800

    小改进

---
+++ p1/testOne.c
@@ -0,0 +1,9 @@
+#include<stdio.h>                
+int main(void)                  
+{
+    int number;            
+    number=2014;               
+    printf("Hello ! I am dotcpp.com\n");    
+    printf("This year is %d\n",number);
+    return 0;
+} 
